3ab61101c52a686694aa572588e2ae2774b3ceb0
bsc-com-06-17 <bsc-com-06-17@cc.ac.mw>
2021-08-02 12:28:02 +0200

    update1

--- app/src/main/java/com/example/tenantfinder/TenantDiscoverFragment.java
+++ app/src/main/java/com/example/tenantfinder/TenantDiscoverFragment.java
@@ -42,6 +42,7 @@ public class TenantDiscoverFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
